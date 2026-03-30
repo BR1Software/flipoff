ce16912dd74aa2bcbac450e71a0669083f960092
Paul <paul@br1software.co.uk>
2026-03-30 11:39:06 +0100

    Update motivational messages in constants.js

--- js/constants.js
+++ js/constants.js
@@ -22,44 +22,16 @@ export const ACCENT_COLORS = [
 export const MESSAGES = [
   [
     '',
-    'GOD IS IN',
-    'THE DETAILS .',
-    '- LUDWIG MIES',
+    'My Goodness',
+    'Look at You',
+    'You are lovely',
     ''
   ],
-  [
-    '',
-    'STAY HUNGRY',
-    'STAY FOOLISH',
-    '- STEVE JOBS',
-    ''
-  ],
-  [
-    '',
-    'GOOD DESIGN IS',
-    'GOOD BUSINESS',
-    '- THOMAS WATSON',
-    ''
-  ],
-  [
-    '',
-    'LESS IS MORE',
-    '',
-    '- MIES VAN DER ROHE',
-    ''
-  ],
-  [
-    '',
-    'MAKE IT SIMPLE',
-    'BUT SIGNIFICANT',
-    '- DON DRAPER',
-    ''
-  ],
-  [
-    '',
-    'HAVE NO FEAR OF',
-    'PERFECTION',
-    '- SALVADOR DALI',
-    ''
-  ]
+    [
+      '',
+        'Hi and Welcome',
+        'to Stevensons',
+        '- Enjoy -',
+        ''
+    ]
 ];
